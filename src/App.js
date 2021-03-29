12f2448cfb56b7bf4096779ffceac21e07119aa9
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2021-03-29 01:04:29 +0100

    fixed some map bugs

--- src/App.js
+++ src/App.js
@@ -25,20 +25,18 @@ function App() {
         },
       });
       OneSignal.showNativePrompt();
-      userData && OneSignal.setExternalUserId(userData._id);
-      !userData && OneSignal.removeExternalUserId();
     });
   
   },[])
 
-  // React.useEffect(()=>{
-  //   userData && OneSignal.push(function() {
-  //     OneSignal.setExternalUserId(userData._id);
-  //   });
-  //   !userData && OneSignal.push(function() {
-  //     OneSignal.removeExternalUserId();
-  //   });
-  // },[userData,OneSignal])
+  React.useEffect(()=>{
+  
+     userData && OneSignal.setExternalUserId(userData._id);
+  
+  
+     !userData && OneSignal.removeExternalUserId();
+  
+  },[userData])
 
   return (
 <>
