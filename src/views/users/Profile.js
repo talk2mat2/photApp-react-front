825552266bd53754d15f470eaf28c6088adb36c1
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2021-03-29 14:08:20 +0100

    worked on the support mesSAGE FEATURE

--- src/views/users/Profile.js
+++ src/views/users/Profile.js
@@ -186,9 +186,9 @@ const Profile = (props) => {
 								labelId='demo-simple-select-label'
 								id='demo-simple-select'
 							>
-								<MenuItem value={10}>Ten</MenuItem>
+								{/* <MenuItem value={10}>Ten</MenuItem>
 								<MenuItem value={20}>Twenty</MenuItem>
-								<MenuItem value={30}>Thirty</MenuItem>
+								<MenuItem value={30}>Thirty</MenuItem> */}
 							</Select>
 						</FormControl>
 					</Grid>
